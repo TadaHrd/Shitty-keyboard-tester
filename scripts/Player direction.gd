0abfb73174056cc25d0ee45eb2990a5726d4a0ab
tadaHrd <hrudka.tadeas@gmail.com>
2022-12-31 13:07:53 +0100

    Added all the files

---
+++ scripts/Player direction.gd
@@ -0,0 +1,26 @@
+extends Control
+
+onready var rect = $TextureRect
+
+func _physics_process(delta):
+	var y = Input.get_action_strength("w") - Input.get_action_strength("s")
+	var x = Input.get_action_strength("d") - Input.get_action_strength("a")
+	
+	if x == 0 and y == 0: rect.visible = false #Sorry for this #I can't say how sorry i am for this
+	else:                                      #I know nesting is bad but i don't know how to make this
+		rect.visible = true                    #without nesting #Somebody please fix this
+		if y == 1 and x == 0: rect.set_rotation_degrees(0)
+		else:
+			if y == 1 and x == 1: rect.set_rotation_degrees(45)
+			else:
+				if y == 0 and x == 1:  rect.set_rotation_degrees(90)
+				else:
+					if y == -1 and x == 1: rect.set_rotation_degrees(135)
+					else:
+						if y == -1 and x == 0: rect.set_rotation_degrees(180)
+						else:
+							if y == -1 and x == -1: rect.set_rotation_degrees(-135)
+							else:
+								if y == 0 and x == -1: rect.set_rotation_degrees(-90)
+								else:
+									if y == 1 and x == -1: rect.set_rotation_degrees(-45)
---
+++ scripts/X.gd
@@ -0,0 +1,18 @@
+extends Control
+
+var pressed: bool
+
+onready var normal = $normal
+onready var inverse = $pressed
+
+func _input(event):
+	if Input.get_action_strength("x") == 1: pressed = true; else: pressed = false
+
+func _physics_process(delta):
+	if pressed == true:
+		normal.visible = false
+		inverse.visible = true
+	else:
+		normal.visible = true
+		inverse.visible = false
+	
